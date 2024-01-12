bc8eba92c998f43ad3e269de6d45bb9686ba3ce3
KhaysK <k.khaysadykov@innopolis.university>
2024-01-13 00:47:51 +0300

    Add Feed and PromtCard components

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+'use client';
+
+import { useState, useEffect } from 'react'
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({ prompts, handleTagClick }) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {prompts.map((prompt) => (
+        <PromptCard
+          key={prompt._id}
+          prompt={prompt}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  )
+}
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+  const [prompts, setPrompts] = useState([]);
+
+  const handleSearchChange = (e) => {
+
+  }
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+
+      setPrompts(data);
+    }
+
+    fetchPosts();
+  }, [])
+
   return (
-    <div>Feed</div>
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='Search for a tag or a username'
+          value={searchText}
+          onChange={handleSearchChange}
+          className='search_input peer'
+          required
+        />
+      </form>
+
+      <PromptCardList
+        prompts={prompts}
+        handleTagClick={() => { }}
+      />
+    </section>
   )
 }
 
